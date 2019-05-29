d18edc640c75f59d5d8244d4950565aa1db77c29
Jocokomo <50770748+Jocokomo@users.noreply.github.com>
2019-05-29 15:22:14 +0200

    Add files via upload

--- amoba.py
+++ amoba.py
@@ -7,7 +7,7 @@
 # AI
 
 
-def moves():
+def moves(tie_sit,win_cond):
     def p1():
         try:
             player1y = int(input("PLAYER ONE select y position:  "))-1
@@ -18,7 +18,7 @@ def moves():
             else:
                 print("wrong placement")
                 p1()
-        except ValueError:
+        except (ValueError,IndexError):
             print("The input you gave was wrong")
             p1()
     def p2():
@@ -28,55 +28,75 @@ def moves():
             if board[player2y][player2x]=="0":
                 board[player2y][player2x]="o"
                 printing_board(board)
-                
             else:
                 print("wrong placement")
                 p2()
-        except ValueError:
+        except (ValueError,IndexError):
             print("The input you gave was wrong")
             p2()
-    while Win_condition is False:
+    while tie_sit != True and win_cond !=True:
         p1()
+        tie_sit = tie(board)
+        win_cond = victory_in_row(board)
+        if tie_sit == True:
+            print("Tie")
+            break
+        if win_cond == True:
+            print("victory")
+            break
         p2()
-        
+        tie_sit = tie(board)
+        win_cond1 = victory_in_row(board)
+        win_cond2 = victory_in_column(board)
+       #win_cond3 = victory_in_diagonal(board)
+        if tie_sit == True:
+            print("Tie")
+            break
+        if (win_cond1 == True) or (win_cond2 == True): # or (win_cond3 == True):
+            print("victory")
+            break
                 
-# def victory(x):
-#     def victory_in_row(x):
-        
-#     # def victory_in_column():
-#     # def victory_in_diagonal():
+def victory_in_row(board):
+    for row in range(len(board)):
+        for i in board:
+            if ["x","x","x"] in board:
+                print("X won")
+                return True
+            elif ["o","o","o"] in board:
+                print("O won")
+                return True
+    return False
 
-#     # victory_row = False
-#     # victory_column = False
-#     # victory_diagonal = False
-#     # victory_in_row()
-#     # victory_in_column()
-#     # victory_in_diagonal()       
+def victory_in_column(board):
+    # for j,column in enumerate(board):
+    #     for k,i in enumerate(column):
+    if ((board[0][0] == "x") and (board[1][0] == "x") and (board[2][0] == "x")):
+        print("victory")
+        return True
+
+def tie(x):
+    for item in x:
+        if "0" in item:
+            return False
+    print("Board is full")
+    return True
 
 def printing_board(x):
     for elem in board:
-        print(" | ".join(elem))
+        if elem == 0:
+            print("-------------")
+        else:
+            print("-------------")
+            print("| "+" | ".join(elem)+" |")
+    print("-------------")   
+# size = input("size")
+# block = []
+# for block in range(size):
 
-        
-            
-
-block = []
-board = []#block for i in range(size)
-
-size = int(input("gimme a size: "))   
-for sor in range (size):
-    block.append("0")
-    board.append(block)
-    board.append("-"*size)
-    
-
-
-#[["0","0","0"],["0","0","0"],["0","0","0"]]#
-Win_condition = False
-
+board = [["0","0","0"],
+        ["0","0","0"],
+        ["0","0","0"]]#block for i in range(size)
+tie_situation = None
+win_condition = None
 printing_board(board)
-
-moves()
-#victory(board)
-
-
+moves(tie_situation,win_condition)
